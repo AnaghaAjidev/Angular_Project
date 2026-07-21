e42aaf6c83ee76c54c94d839b9e98ae8bd11d13f
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-21 12:19:10 +0530

    Commit new changes

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('campus360',[]);
+
+    app.controller("dashboardcontroller",function($scope) {
+
+
+        $scope.collegename="FISAT";
+        $scope.totalstudents="4500";
+        $scope.totalfaculty="1500";
+
+        console.log("dashboard controller is loaded");
+
+        $scope.students=[
+            "anagha","ananya","arya","jyothika"
+        ];
+
+        $scope.addstudents=function(){
+
+            $scope.totalstudents++;
+        };
+
+    });
