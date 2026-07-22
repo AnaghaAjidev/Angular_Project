fc879086a3e1e0c1dda484a4f47e5e7d2dd69383
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-22 11:39:00 +0530

    ng-switch, filters, ng-models added

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
-var app = angular.module('campus360',[]);
+var app = angular.module('campus360', []);
 
-    app.controller("dashboardcontroller",function($scope) {
+app.controller("dashboardcontroller", function ($scope) {
 
 
-        $scope.collegename="FISAT";
-        $scope.totalstudents="4500";
-        $scope.totalfaculty="1500";
+    $scope.collegename = "FISAT";
+    $scope.totalstudents = 1200;
+    $scope.totalfaculty = 1500;
 
-        console.log("dashboard controller is loaded");
+    console.log("dashboard controller is loaded");
 
-        $scope.students=[
-            "anagha","ananya","arya","jyothika"
+    $scope.students = [
+        "anagha", "ananya", "arya", "jyothika"
+    ];
+
+  
+        $scope.showstudents = false;
+        $scope.showDashboard = true;
+
+        $scope.fees = 25000;
+        $scope.today = new Date();
+
+        $scope.departments=[
+            "CSE","MCA","MBA","MCOM","ECE","IMCA","BCA"
         ];
 
-        $scope.addstudents=function(){
+        $scope.selectedDept = "MCA";
 
-            $scope.totalstudents++;
-            $scope.showstudents=true;
-            $scope.showDashboard=true;
-        };
+    $scope.addstudents = function () {
+        $scope.totalstudents++;
+    };
 
-    });
+});
